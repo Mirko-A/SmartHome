c82fda2d950b5e6d9ed29ce128c2a63b8d36171e
Mirko Adzic <adzicmirko97@gmail.com>
2026-04-15 11:15:15 +0200

    add tui mode stub

--- SmartHome/main.cpp
+++ SmartHome/main.cpp
@@ -1,12 +1,54 @@
+#if 0
 #include <QApplication>
 
 #include "mainwindow.h"
+#endif
 
-int main(int argc, char *argv[]) {
+#include <iostream>
+#include <string>
+
+int gui_main(int argc, char *argv[]) {
+#if 0
     QApplication app(argc, argv);
     MainWindow window;
     window.setWindowTitle("Smart Home");
 
     window.show();
     return app.exec();
+#else
+    std::cout << "GUI mode stub" << std::endl;
+    return 0;
+#endif
+}
+
+int tui_main(int argc, char *argv[]) {
+    std::cout << "TUI mode stub" << std::endl;
+    for (int i = 0; i < argc; ++i) {
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <mode>" << std::endl;
+        std::cerr << "Modes: gui, tui" << std::endl;
+        return 1;
+    }
+
+    // Copy the mode argument, then remove it from argv.
+    std::string mode = argv[1];
+    for (int i = 2; i < argc; ++i) {
+        argv[i - 1] = argv[i];
+    }
+    argc -= 1;
+
+    if (mode == "gui") {
+        return gui_main(argc, argv);
+    } else if (mode == "tui") {
+        return tui_main(argc, argv);
+    } else {
+        std::cerr << "Unknown mode: " << mode << std::endl;
+        return 1;
+    }
 }
