4a162eff34446f887092d27ee894b29b9883f62a
Mirko Adzic <adzicmirko97@gmail.com>
2026-04-18 13:20:36 +0200

    core: hal singleton

--- SmartHome/Core/hal.cpp
+++ SmartHome/Core/hal.cpp
@@ -1,10 +1,12 @@
 #include "hal.h"
 
 // #include <wiringPi.h>
-// #include <softPwm.h>
+
+static int wiringPiSetup() {
+    // Stub: Assume success for now.
+    return 0;
+}
 
 HAL::HAL() {
-    // m_InitOK = (wiringPiSetup() >= 0);
-    //  TODO: change first arg, this will be used for the motor
-    // m_InitOK = (softPwmCreate(28, 0, 100)!=0);
+    m_Initialized = (wiringPiSetup() >= 0);
 };
--- SmartHome/Core/hal.h
+++ SmartHome/Core/hal.h
@@ -3,20 +3,32 @@
 
 #include <cstdint>
 
-// TODO: HAL not done yet, still need to implement all functions
 class HAL {
   public:
+    static HAL &instance() {
+        static HAL hal;
+        return hal;
+    }
+
+    HAL(const HAL &) = delete;
+    HAL &operator=(const HAL &) = delete;
+
+    void pinMode(uint8_t pin, uint8_t mode);
+
+    uint8_t digitalRead(uint8_t pin);
+    void digitalWrite(uint8_t pin, uint8_t value);
+
+    int analogRead(uint8_t pin);
+
+    void pwmWrite(uint8_t pin, uint16_t value);
+
+    bool readDHT22(uint8_t pin, float &temp, float &humidity);
+
+  private:
     HAL();
     ~HAL();
 
-    uint8_t digitalRead(uint8_t pin);
-    void digitalWrite(uint8_t value, uint8_t pin);
-
-    void createPWM(uint8_t pin);
-    void sendPWM(uint8_t value, uint8_t pin);
-
-  private:
-    bool m_InitOK;
+    bool m_Initialized;
 };
 
 #endif // HAL_H
